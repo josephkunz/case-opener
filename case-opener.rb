9004b8f58d66f4a7774ed79ac03356c9fc41bb77
Joseph Kunz <joseph.kunz@gmx.de>
2021-02-04 20:42:50 +0100

    implements loop

--- case-opener.rb
+++ case-opener.rb
@@ -1,11 +1,21 @@
 def case_opener
-  puts "Insert case in the following format: c-123/24"
-  print ">>>> "
-  case_id = gets.chomp
-  puts case_id
+  puts "Insert case number in the following format: 123/24. When done, type: done."
+  print ">>>> C-"
+  case_id = gets.chomp.capitalize
+  until case_id == "Done" do
+    puts "Insert case number in the following format: 123/24."
+    print ">>>> C-"
+    case_id = gets.chomp.capitalize
+    puts case_id
 
-  puts "the case for #{case_id} will be opened in the browser!"
-  system("open", "http://curia.europa.eu/juris/liste.jsf?language=en&num=#{case_id}")
+    puts "the case for #{case_id} will be opened in the browser!"
+    system("open", "http://curia.europa.eu/juris/liste.jsf?language=en&num=C-#{case_id}")
+  end
+  puts "Have fun with the case(s)!"
 end
 
 case_opener
+
+
+# to implement:
+# 1. while user input not "done" ask for user input and open user input
